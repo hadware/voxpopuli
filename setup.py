75c5e1ad9b99b53d6a9e9f3276914c61ccbfb793
hadware <hadwarez@gmail.com>
2017-01-30 03:41:08 +0100

    Edited config files and uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 # coding=utf-8
 """Install config."""
 from setuptools import setup, find_packages
+import os
+
+long_description = '''A wrapper around Espeak and Mbrola, to do simple Text-To-Speech (TTS),
+    with the possibility to tweak the phonemic form.'''
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
 
 setup(
     name='voxpopuli',
@@ -14,7 +20,7 @@ setup(
     classifiers=[
         'Topic :: Text Processing :: Linguistic',
         'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
-        'Topic :: Multimedia :: Sound/Audio :: Speech'
+        'Topic :: Multimedia :: Sound/Audio :: Speech',
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
