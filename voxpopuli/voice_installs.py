c4861ea5ed5bf26db8c5edaf7eaff51c7665659a
Hadrien Titeux <hadwarez@gmail.com>
2018-01-31 01:29:24 +0100

    Added installer support for spanish, german and english voices

--- voxpopuli/voice_installs.py
+++ voxpopuli/voice_installs.py
@@ -2,6 +2,7 @@ from urllib import request
 from zipfile import ZipFile
 from io import BytesIO
 from os import makedirs
+import re
 
 BASE_URL = "http://tcts.fpms.ac.be/synthesis/mbrola/dba/"
 MBROLA_FOLDER = "/usr/share/mbrola/"
@@ -13,24 +14,41 @@ LANG_FILES = {"fr": ["fr1/fr1-990204.zip",
                      "fr5/fr5-991020.zip",
                      "fr6/fr6-010330.zip",
                      "fr7/fr7-010330.zip"],
-              "en": [],
-              "us": [],
-              "es": [],
-              "de": []}
+              "en": ["en1/en1-980910.zip"],
+              "us": ["us1/us1-980512.zip",
+                     "us2/us2-980812.zip",
+                     "us3/us3-990208.zip"],
+              "es": ["es1/es1-980610.zip",
+                     "es2/es2-989825.zip",
+                     "es3/es3.zip",
+                     "es4/es4.zip"],
+              "de": ["de1/de1-980227.zip",
+                     "de2/de2-990106.zip",
+                     "de3/de3-000307.zip",
+                     "de4/de4.zip",
+                     "de5/de5.zip",
+                     "de6/de6.zip",
+                     "de7/de7.zip",
+                     "de8/de8.zip"]}
+
+
+def create_folder_and_extract(voice_name, zfile):
+    try:
+        makedirs(MBROLA_FOLDER + voice_name + "/")
+    except FileExistsError:
+        pass
+    zfile.extract(voice_name, MBROLA_FOLDER + voice_name + "/")
+
 
 def install_voices(lang="fr"):
     """Automatically downloads and extracts all the voices for one language in the /usr/share/mbrola folder"""
     for file in LANG_FILES[lang]:
         print("Downloading file %s" % file)
         zipfile = request.urlopen(BASE_URL + file).read()
-        filename = file.split("-")[0]
+        filename = re.match(r"[a-z]{2}[1-9]/[a-z]{2}[1-9]", file).group()
         print("Extracting file")
         with ZipFile(BytesIO(zipfile), "r") as zfile:
-            if "fr4" in filename:
-                try:
-                    makedirs(MBROLA_FOLDER + "fr4/")
-                except:
-                    pass
-                zfile.extract("fr4", MBROLA_FOLDER + "fr4/")
+            if filename.split("/")[0] in ["fr4", "de4", "de7"]:
+                create_folder_and_extract(filename.split("/")[0], zfile)
             else:
-            zfile.extract(filename, MBROLA_FOLDER)
+                zfile.extract(filename, MBROLA_FOLDER)
