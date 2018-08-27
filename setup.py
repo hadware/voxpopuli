3efc0343920249c2fcf1ece894db524bc790ec8d
hadware <hadware@gmail.com>
2018-08-27 20:44:32 +0200

    Import fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if os.path.exists('README.rst'):
 
 setup(
     name='voxpopuli',
-    version='0.1.5.0',
+    version='0.1.5.1',
     description='A wrapper around Espeak and Mbrola, to do simple Text-To-Speech (TTS),'
                 ' with the possibility to tweak the phonemic form.',
     url='https://github.com/hadware/voxpopuli',
